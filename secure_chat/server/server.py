a59087b0f8e03d8f324797066eb5ce324dce8b35
Arsalan <arsalan.vahi2009@gmail.com>
2026-04-23 01:10:01 +0300

    server.py class skeleton adding

--- secure_chat/server/server.py
+++ secure_chat/server/server.py
@@ -14,14 +14,135 @@
 # =========================================
 # 4. Runtime State
 # =========================================
+class ServeSessionManager:
+    def __init__(self):
+        self.active_connections = None
+        self.authenticated_sessions = None
+        self.online_users = None
+        self.username_to_session_mapping = None
+        self.connection_to_user_mapping = None
+        self.channel_participants = None
+    def add_connections(self):
+        pass
+    def remove_connections(self):
+        pass
+    def bind_connectio_to_identy(self):
+        pass
+    def mark_session_authenticated(self):
+        pass
+    def check_whether_username_is_active(self):
+        pass
+    def get_session_by_identifier(self):
+        pass
+    def get_connected_clients(self):
+        pass
+    def get_authenticated_clients(self):
+        pass
+    def resolve_recipients_for_channel(self):
+        pass
+    def clear_all_sessions(self):
+        pass
+    
 
 # =========================================
 # 5. Security
 # =========================================
+class ServerCryptoService:
+    def __init__(self):
+        self.loaded_rsa_encryption_key_pair = None
+        self.loaded_rsa_signing_key_pairs = None
+        self.key_validity_status = False
+        self.crypto_readiness_status = False
+
+    def load_rsa_keys(self):
+        pass
+
+    def validate_rsa_keys(self):
+        pass
+
+    def decrypt_registration_payload(self):
+        pass
+
+    def generate_secure_challenge(self):
+        pass
+
+    def verify_challenge_response(self):
+        pass
+
+    def derive_response_protection_material(self):
+        pass
+
+    def encrypt_authentication_result(self):
+        pass
+
+    def sign_response(self):
+        pass
+
+    def derive_channel_key_material(self):
+        pass
+
 
 # =========================================
 # 6. Transport / Lifecycle
 # =========================================
+class ServerTransportManager:
+    def __init__(self):
+        self.listening_socket = None
+        self.accept_loop_state = None
+        self.active_connection_handler_set = set()
+        self.packet_dispatch_registration = None
+        self.transport_health_state = None
+    def start_accepting_client_connections(self):
+        pass
+    def stop_accepting_client_connections(self):
+        pass
+    def receive_client_packet(self):
+        pass
+    def dispatch_incoming_packet(self):
+        pass
+    def send_response_to_client(self):
+        pass
+    def broadcast_packet_to_recipients(self):
+        pass
+    def detect_client_disconnect(self):
+        pass
+    def register_transport_handler(self):
+        pass
+    def open_session(self):
+        pass
+    def close_session(self):
+        pass
+    def send_application_message(self):
+        pass
+    def receive_application_message(self):
+        pass
+class ServerLifecycleManager:
+    def __init__(self):
+        self.lifecycle_phase = None
+        self.startup_in_progress = False
+        self.shutdown_in_progress = False
+        self.last_lifecycle_result = None
+        self.last_lifecycle_error = None
+    def validate_startup_request(self):
+        pass
+    def initialize_runtime(self):
+        pass
+    def bind_and_listen(self):
+        pass
+    def enter_running_state(self):
+        pass
+    def begin_shutdown(self):
+        pass
+    def stop_accepting_new_connections(self):
+        pass
+    def terminate_active_sessions(self):
+        pass
+    def release_runtime_resources(self):
+        pass
+    def finalize_shutdown(self):
+        pass
+    def get_lifecycle_state(self):
+        pass
 
 # =========================================
 # 7. Application / Service Logic
@@ -43,6 +164,102 @@ class ServerAppCoordinator:
         pass
     def get_server_status(self):
         pass
+class RegistrationService:
+    def __init__(self):
+        self.current_registration_request_context = None
+        self.current_decrypted_registration_payload = None
+        self.last_registration_result = None
+        self.last_registration_error = None
+    def handle_registration_request(self):
+        pass
+    def decrypt_registration_payload(self):
+        pass
+    def valid_registration_payload(self):
+        pass
+    def check_username_availability(self):
+        pass
+    def save_enrollment_record(self):
+        pass
+    def create_registration_result(self):
+        pass
+    def sign_registration_result(self):
+        pass
+    def send_registration_response(self):
+        pass
+class AuthenticationServe:
+    def __init__(self):
+        self.current_authentication_request_context = None
+        self.current_challenge = None
+        self.current_authentication_result = None
+        self.last_authentication_error = None
+    def handle_authentication_request(self):
+        pass
+    def validate_authentication_eligibility(self):
+        pass
+    def generate_authenticate_challenge(self):
+        pass
+    def send_authentication_challenge(self):
+        pass
+    def receive_authentication_response(self):
+        pass
+    def verify_authentication_response(self):
+        pass
+    def determine_authentication_outcome(self):
+        pass
+    def build_authentication_result(self):
+        pass
+    def protect_authentication_result(self):
+        pass
+    def active_authenticated_session(self):
+        pass
+    def send_authentication_result(self):
+        pass
+class MessageReplayService:
+    def __init__(self):
+        self.current_relay_context = None
+        self.last_relay_result  = None
+        self.last_routing_error = None
+    def validate_sender_for_routing(self):
+        pass
+    def resolve_channel_recipients(self):
+        pass
+    def relay_secure_packet(self):
+        pass
+    def broadcast_packet_unchanged(self):
+        pass
+    def handle_recipient_disconnect_during_relay(self):
+        pass
+    def record_relay_event(self):
+        pass
+    def notify_traffic_monitor(self):
+        pass
+class ChannelKeyManager:
+    def __init__(self):
+        self.per_channel_aes_keys = None
+        self.per_channel_ivs = None
+        self.per_channel_hmac_keys = None
+        self.channel_available_flags = False
+        self.key_generation_status = False
+    def validate_channel_key_generation_request(self):
+        pass
+    def generate_channel_keys(self):
+        pass
+    def install_channel_keys(self):
+        pass
+    def retrieve_channel_keys(self):
+        pass
+    def check_channel_availability(self):
+        pass
+    def mark_channel_available(self):
+        pass
+    def mark_channel_unavailable(self):
+        pass
+    def clear_channel_keys(self):
+        pass
+
+
+
+
 
 # =========================================
 # 8. Monitoring
